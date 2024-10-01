c4d738fadfb373740649b7ee7bfac2a2e8380830
crow_ak <c0b230019c@edu.teu.ac.jp>
2024-10-01 14:12:25 +0900

    練習1 移動量辞書の定義

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,12 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    DELTA = {
+        pg.K_UP:(0, -5),
+        pg.K_DOWN:(0, 5),
+        pg.K_LEFT:(-5, 0),
+        pg.K_RIGHT:(5, 0)
+        }
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
@@ -24,14 +30,18 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        for key, tpl in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += tpl[0] # 横方向
+                sum_mv[1] += tpl[1] # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
